96c1df4c7fa6f94b7937e810d09853aec208faad
yezihdw <yezihdw@sina.com>
2018-10-14 10:06:46 +0800

    队列的数组实现

---
+++ ArrayQueue.java
@@ -0,0 +1,38 @@
+// 用数组实现的队列
+public classs ArrayQueue {
+   // 数组：items, 数组大小：n
+   private String[] items;
+   private int n = 0;
+   // head 表示队头下标, tail 表示队尾下标
+   private int head = 0;
+   private int tail = 0;
+
+   // 申请一个大小为 capacity 的数组
+   public ArrayQueue(int capacity) {
+     itmes = new String[capacity];
+     n = capacity;
+   }
+
+   // 入队
+   public boolean enqueue(String item) {
+     // 如果 tail == n 表示对列已经满了
+     if (tail == n) {
+       return false;
+     }
+     items[tail] = item;
+     ++ tail;
+     return true;
+   }
+
+   // 出队
+   public String dequeue() {
+     // 如果 head == tail 表示队列为空
+     if (head == tail) {
+       return null;
+     }
+     String ret = items[head];
+     ++ head;
+     return ret;
+   }
+
+}
